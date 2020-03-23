9da198541f98bbc9695ae86ec7c9f6bec33270f6
eyal23 <eyal.heinrich.2003@gmail.com>
2020-03-23 11:26:08 +0200

    added printing of developer, date and time

--- Gallery.cpp
+++ Gallery.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <ctime>
+
 #include "MemoryAccess.h"
 #include "AlbumManager.h"
 
@@ -36,6 +38,10 @@ int main(void)
 	// initialize album manager
 	AlbumManager albumManager(dataAccess);
 
+	char date[256];
+	time_t ttime = time(0);
+	strftime(date, sizeof(date), "time: %b/%d/%Y\nhour: %H", localtime(&ttime));
+	std::cout << "Developer: Eyal Heinrich" << std::endl << date << std::endl << std::endl;
 
 	std::string albumName;
 	std::cout << "Welcome to Gallery!" << std::endl;
